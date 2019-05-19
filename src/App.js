a7f977a786052b78888d235f07033d7b37a7123e
Bryan Bethea <bryanbethea28@gmail.com>
2019-05-19 06:38:40 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import gql from 'graphql-tag';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import styled from 'styled-components';
+
+const allLiftsQuery = gql`
+	query {
+		allLifts {
+			id
+			name
+			status
+			capacity
+		}
+	}
+`;
+
+const liftStatusMutatiion = gql`
+	mutation($id: ID!, $status: LiftStatus!) {
+		setLiftStatus(id: $id, status: $status) {
+			id
+			name
+			status
+		}
+	}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { loading, data } = useQuery(allLiftsQuery);
+	const [setStatus] = useMutation(liftStatusMutatiion);
+
+	console.log('test', setStatus);
+	return (
+		<div>
+			<h1>snowtooth lift status</h1>
+			{loading && <p>Loading</p>}
+			{data && !loading ? (
+				<table>
+					<tbody>
+						{data.allLifts.map(lift => (
+							<tr>
+								<td>{lift.name}</td>
+								<td>{lift.status}</td>
+								<td>
+									<StatusIndicator
+										status={lift.status}
+										onSelect={status =>
+											setStatus({
+												variables: {
+													id: lift.id,
+													status,
+												},
+											})
+										}
+									/>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			) : null}
+		</div>
+	);
 }
+
+const StatusIndicator = ({ status, onSelect }) => (
+	<>
+		<Circle color="green" selected={status === 'OPEN'} onClick={() => onSelect('OPEN')} />
+		<Circle color="yellow" selected={status === 'HOLD'} onClick={() => onSelect('OPEN')} />
+		<Circle color="red" selected={status === 'CLOSE'} onClick={() => onSelect('CLOSE')} />
+	</>
+);
+const Circle = styled.div`
+	border-radius: 50%
+  border: solid 1px black
+  width: 20px
+  height: 20px
+  float: left 
+ 
+	
+`;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,78 @@
+// import { request } from 'graphql-request';
+// import ApolloClient, { gql } from 'apollo-boost';
+import ApolloClient from 'apollo-client';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-boost';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Vote from './vote';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const link = createHttpLink({ uri: 'https://snowtooth.moonhighway.com' });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+	link,
+	cache,
+});
+console.log('client', client.cache.data);
+render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
+);
+// import gql from 'graphql-tag';
+// import { InMemoryCache } from 'apollo-cache-inmemory';
+// let url = 'https://snowtooth.moonhighway.com';
+
+// const query = gql`
+// 	query {
+// 		allLifts {
+// 			name
+// 		}
+// 	}
+// `;
+
+// const mutation = gql`
+// 	mutation($id: ID!, $status: LiftStatus!) {
+// 		setLiftStatus(id: $ID, status: $status) {
+// 			name
+// 			status
+// 		}
+// 	}
+// `;
+// const client = new ApolloClient({
+// 	uri: 'https://snowtooth.moonhighway.com',
+// 	// cache,
+// });
+
+// const lifts = async () => {
+// 	await client.query({ query });
+// 	let readCache = client.readQuery({ query });
+// 	console.log('readcache', readCache);
+// };
+
+// lifts();
+
+// client.query({ query }).then(console.log);
+// client.mutate({ mutation, variables: { id: 'panorama', status: 'HOLD' } });
+
+// console.log('CACHE', client.cache.data.data);
+// let query = `${url}`;
+
+// let opt = {
+// 	method: 'POST',
+// 	headers: { 'Content-Type': 'application/json' },
+// 	body: JSON.stringify({ query }),
+// };
+
+// fetch(url, opt)
+// 	.then(res => res.json())
+// 	.then(({ data }) => `<p>Lifts: ${data.allLifts.map(lift => lift.name)}</p>`)
+// 	.then(text => (document.body.innerHTML = text));
+
+// .request(url, query)
+// .then(console.log);
